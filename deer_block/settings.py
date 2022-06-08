917d2a771efda02dac128fed4cc4a9cba89c0eda
老许 <1143841809@qq.com>
2022-06-09 00:41:26 +0800

    fix

--- deer_block/settings.py
+++ deer_block/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middlewares.logger_middleware.LogMiddle',
 ]
 
 ROOT_URLCONF = 'deer_block.urls'
